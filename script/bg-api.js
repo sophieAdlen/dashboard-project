c1de654eb301227b3818e5755dfc5605b67641a8
sophie adlen <sophie.adlen@chasacademy.se>
2024-01-16 12:09:06 +0100

    weather api done

--- script/bg-api.js
+++ script/bg-api.js
@@ -1,28 +1,28 @@
-// import axios from "axios";
+import axios from "axios";
 
-// // Variables
-// const unsplashKey = "iG4ea27tXkAxPtAgMOHooqIp0gly7EXIl-JX0uA6xYU";
-// const url = `https://api.unsplash.com/photos/random/?client_id=${unsplashKey}`;
-// const htmlElement = document.querySelector("#unsplashRandom");
+// Variables
+const unsplashKey = "iG4ea27tXkAxPtAgMOHooqIp0gly7EXIl-JX0uA6xYU";
+const url = `https://api.unsplash.com/photos/random/?client_id=${unsplashKey}`;
+const htmlElement = document.querySelector("#unsplashRandom");
 
-// // Funktion för att hämta en ny bakgrundsbild från Unsplash
-// async function getUser(url) {
-//   try {
-//     const response = await axios.get(url);
-//     console.log(response.data.urls.regular);
-//     htmlElement.style.background = `url(${response.data.urls.regular}) no-repeat center center fixed`;
-//     htmlElement.style.backgroundSize = "cover";
-//     htmlElement.style.height = "100vh";
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+// Funktion för att hämta en ny bakgrundsbild från Unsplash
+async function getUser(url) {
+  try {
+    const response = await axios.get(url);
+    console.log(response.data.urls.regular);
+    htmlElement.style.background = `url(${response.data.urls.regular}) no-repeat center center fixed`;
+    htmlElement.style.backgroundSize = "cover";
+    htmlElement.style.height = "100vh";
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// getUser(url);
+getUser(url);
 
-// // Lägg till klicklyssnare för knappen
-// const bgImageBtn = document.getElementById("bg-image-btn");
+// Lägg till klicklyssnare för knappen
+const bgImageBtn = document.getElementById("bg-image-btn");
 
-// bgImageBtn.addEventListener("click", function() {
-//   getUser(url);
-// });
+bgImageBtn.addEventListener("click", function() {
+  getUser(url);
+});
--- script/weather-api.js
+++ script/weather-api.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -7,44 +7,53 @@ function getLocation() {
     console.log("Geolocation is not supported by this browser.");
   }
 }
+
 function showPosition(position) {
   const lat = position.coords.latitude;
   const long = position.coords.longitude;
-//   console.log(lat, long);
-  fetchData(lat, long)
+  fetchData(lat, long);
 }
+
 getLocation();
 
 async function fetchData(lat, long) {
-    const APIkey = "49a051eadf02f74befae243a2fc60996";
-    const endpoint = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APIkey}&units=metric`;
-  
-    try {
-      const response = await axios.get(endpoint);
-  
-      const temperature = Math.round(response.data.list[0].main.temp) + "°C";
-      const date = new Date(response.data.list[0].dt_txt).toLocaleDateString('sv-SE', { weekday: 'long' });
+  const APIkey = "49a051eadf02f74befae243a2fc60996";
+  const endpoint = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${APIkey}&units=metric`;
+
+  try {
+    const response = await axios.get(endpoint);
+    const weatherList = document.querySelector(".weatherlist");
+
+    function createWeatherListItem(index) {
+      const temperature = Math.round(response.data.list[index].main.temp) + "°C";
+      const date = new Date(response.data.list[index].dt_txt).toLocaleDateString('sv-SE', { weekday: 'long' });
       const capitalizedDate = date.charAt(0).toUpperCase() + date.slice(1);
-      const weatherDescription = response.data.list[0].weather[0].description;
-      const weatherIcon = response.data.list[0].weather[0].icon;
-  
-      
-      const weatherList = document.querySelector(".weatherlist");
+      const weatherDescription = response.data.list[index].weather[0].description;
+      const weatherIcon = response.data.list[index].weather[0].icon;
+
       const weatherListItem = document.createElement("li");
-  
+
       weatherListItem.innerHTML = `
-      <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+        <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
         <div class="text-content">
-      <p class="date">${capitalizedDate}</p> 
-        <div class="secondrow">
-        <p class="temperature">${temperature}</p>
-        <p class="description">${weatherDescription}</p>
+          <p class="date">${capitalizedDate}</p> 
+          <div class="secondrow">
+            <p class="temperature">${temperature}</p>
+            <p class="description">${weatherDescription}</p>
+          </div>
         </div>
-        </div>
-    `;
+      `;
       weatherList.appendChild(weatherListItem);
-      
-    } catch (error) {
-      console.error("Error", error.message);
     }
+
+    // Loopa igenom önskade index (0, 8 och 16)
+    const desiredIndices = [0, 8, 16];
+
+    desiredIndices.forEach(index => {
+      createWeatherListItem(index);
+    });
+
+  } catch (error) {
+    console.error("Error", error.message);
   }
+}
